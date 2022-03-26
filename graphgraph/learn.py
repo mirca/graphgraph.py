5f17a4472984b10ea39a45ac172f24f20af09d76
mirca <jvmirca@gmail.com>
2022-03-26 20:32:14 +0800

    update learn.py

--- graphgraph/learn.py
+++ graphgraph/learn.py
@@ -9,7 +9,7 @@ def learn_laplacian_cvxpy(samples, scale_input=True):
         S = np.cov(samples.T)
     p = np.shape(S)[0]
     L = cp.Variable((p, p), PSD=True)
-    J = (1/p) * np.ones((p, p))
+    J = (1 / p) * np.ones((p, p))
     objective = cp.Minimize(-cp.log_det(L + J) + cp.trace(L @ S))
     constraints = [cp.sum(L, axis=0) == 0, L - cp.diag(cp.diag(L)) <= 0]
     problem = cp.Problem(objective, constraints)
